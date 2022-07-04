ec27e38cbe6266036d9a53ec1c636a566fe7a14c
Prisca <prisca.ros@onja.org>
2022-07-04 10:57:18 +0300

    Added a GithubTable component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GraphQLClient, ClientContext } from 'graphql-hooks'
+import GithubTable from './components/GithubTable';
 
-function App() {
+const client = new GraphQLClient({
+  url: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: "bearer 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"
+  }
+})
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ClientContext.Provider value={client}>
+      <GithubTable />
+    </ClientContext.Provider>
+  )
 }
-
-export default App;
