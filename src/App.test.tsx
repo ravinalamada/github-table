220faca7c69a0cc8dbf2f103a914d7c742335c50
Prisca <prisca.ros@onja.org>
2022-07-04 14:45:35 +0300

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('search loading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Loading...');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('custom search term loads results that includes search term', async() => {
+  render(<App />);
+  const input = 
+  await waitFor(async() => expect(await screen.findByTestId('search-input')).toBeInTheDocument())
+  
+})
