e62a3d2ec83c4a9e99e3b75262e0f60440220f12
Prisca <prisca.ros@onja.org>
2022-07-04 10:59:44 +0300

    Creates a GithubTable component.

---
+++ src/components/GithubTable.tsx
@@ -0,0 +1,59 @@
+import { Table, TableRow } from 'semantic-ui-react'
+import { useQuery } from 'graphql-hooks'
+
+const REPOSITORIES_SEARCH_QUERY = `
+  query{
+    search(query: "react sort:stars", type: REPOSITORY, first: 20) {
+      nodes {... on Repository 
+        { 
+          description    
+          name  
+          url  
+          forkCount    
+          stargazerCount    
+          id    
+        }    
+    } }
+  }
+`
+
+export default function GithubTable() {
+  const { loading, error, data } = useQuery(REPOSITORIES_SEARCH_QUERY, {
+    variables: {
+      limit: 10
+    }
+  })
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Something Bad Happened</p>
+
+  const searchResults = data?.search.nodes;
+
+  return (
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Stars</Table.HeaderCell>
+            <Table.HeaderCell>Forks</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {searchResults.map((search: any, index: number) => {
+            return (
+              <TableRow key={index}>
+                <Table.Cell><a href={search.url}>{search.name}</a></Table.Cell>
+                <Table.Cell>{search.forkCount}</Table.Cell>
+                <Table.Cell>{search.stargazerCount}</Table.Cell>
+              </TableRow>
+            )
+          })}
+          <Table.Row>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div >
+  )
+}
+
