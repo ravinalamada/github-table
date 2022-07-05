81e7f32fae67d85cbb59edbe882c73df6fe66d0f
Prisca <prisca.ros@onja.org>
2022-07-05 08:34:31 +0300

    Add env.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GithubTable from './components/GithubTable';
 const client = new GraphQLClient({
   url: 'https://api.github.com/graphql',
   headers: {
-    Authorization: "bearer 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"
+    Authorization:`bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
   }
 })
 
