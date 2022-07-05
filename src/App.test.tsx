9c3ff6f202224f6100365a455bf88482f3a2e0cc
Prisca <prisca.ros@onja.org>
2022-07-05 11:52:13 +0300

    Add test on keyDown handler and clean code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,54 +1,68 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
-// jest.setTimeout(30000)
 
-// test('search loading', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText('Loading...');
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// test('Input field shows default value "react" on initial load', async() => {
-//   render(<App />);
-//   await waitFor(() => {
-//     const parent = screen.getByTestId('search-input');
-//     expect(parent).toBeInTheDocument();
-//     const input = parent.querySelector('input');
-//     expect(input?.value).toBe('react');
-//   }, {
-//     timeout: 8000
-//   });
-// })
-
-test('test if search is executed after I click on the search icon', async() => {
+test('search loading', () => {
   render(<App />);
-  await waitFor(async() => {
+  const linkElement = screen.getByText('Loading...');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Input field shows default value "react" on initial load', async() => {
+  render(<App />);
+  await waitFor(() => {
     const parent = screen.getByTestId('search-input');
     expect(parent).toBeInTheDocument();
     const input = parent.querySelector('input');
-    // expect(input.value).toBe('') // empty before
+    expect(input?.value).toBe('react');
+  }, {
+    timeout: 8000
+  });
+})
+
+test('test if search is executed after I click on the search icon', async() => {
+  render(<App />);
+  await waitFor(async () => {
+    const parent = await screen.findByTestId('search-input');
+    const input = parent.querySelector('input');
+    
     // @ts-ignore
-  fireEvent.change(input, {target: {value: 'angular'}})
-  expect(input?.value).toBe('angular') 
-  const button = screen.getByTestId('button-search');
-  expect(button).toBeInTheDocument();
-  // fireEvent.click(button)
-  fireEvent(
-    button,
-    new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-    })
-  )
-
-  const tableBody = await screen.findByTestId('table-body');
-  expect(tableBody).toBeInTheDocument();
-
-  // const tableData = tableBody.querySelectorAll('td');
-  // console.log(tableData, 'data');
-  
-  // expect(ta)
+    fireEvent.change(input, { target: { value: 'angular' } })
   }, {
     timeout: 18000
   });
-}, 10000)
+  const button = await screen.findByTestId('button-search');
+  expect(button).toBeInTheDocument();
+  fireEvent.click(button)
+  await waitFor(async () => {
+    const recordName = screen.getByText('angular');
+    expect(recordName).toBeInTheDocument();
+    expect(recordName.outerHTML).toEqual('<a href="https://github.com/angular/angular">angular</a>');
+  }, {
+    timeout: 18000
+  });
+}, 30000)
+
+test('test if search is executed after I press Enter', async () => {
+  render(<App />);
+  await waitFor(async () => {
+    const parent = await screen.findByTestId('search-input');
+    const input = parent.querySelector('input');
+    
+    // @ts-ignore
+    fireEvent.change(input, { target: { value: 'angular' } })
+  }, {
+    timeout: 18000
+  });
+  const parent = await screen.findByTestId('search-input');
+  const input = parent.querySelector('input');
+  // @ts-ignore
+  fireEvent.keyDown(input, { key: "Enter", code: 13, charCode: 13 });
+  
+  await waitFor(async () => {
+    const recordName = await screen.findByText('angular');
+    expect(recordName).toBeInTheDocument();
+    expect(recordName.outerHTML).toEqual('<a href="https://github.com/angular/angular">angular</a>');
+  }, {
+    timeout: 18000
+  });
+}, 30000)
